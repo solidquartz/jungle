466bcd0f8f033e12a82c2c36ccaf23fdb97c6816
Aky Mac Donald <akymacd@gmail.com>
2022-08-01 15:08:09 -0400

    Added login and form

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,14 @@ Rails.application.routes.draw do
 	get 'users/new' => 'users#new', as: :new_user
 	
 	post 'users' => 'users#create'
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+	
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
