c5e514b0af5d16d3e29adf40d7fc1f2f2a3f3de0
Aky Mac Donald <akymacd@gmail.com>
2022-08-02 14:24:47 -0400

    Added password minimum length and more detailed user auth with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
 has_secure_password
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }
 	validates :first_name, presence: true, uniqueness: false
 	validates :last_name, presence: true, uniqueness: false
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,22 +4,28 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
   it "is invalid when password and password_confirmation fields don't match" do
-    @user = User.new(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "password", :password_confirmation => "notpassword")
+    @user = User.new(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "password1", :password_confirmation => "notpassword")
     expect(@user).to_not be_valid
     expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
   end
 
   it "is invalid when the email is not unique" do
-    User.create!(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "password", :password_confirmation => "password")
-    @user = User.new(:first_name => "Cloud", :last_name => "Strife",:email => "email@email.com", :password => "password", :password_confirmation => "password")
+    User.create!(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "password1", :password_confirmation => "password1")
+    @user = User.new(:first_name => "Cloud", :last_name => "Strife",:email => "email@email.com", :password => "password1", :password_confirmation => "password1")
     expect(@user).to_not be_valid
     expect(@user.errors.full_messages[0]).to eq("Email has already been taken")
   end
 
   it "is invalid when first name is not provided" do
-    @user = User.new(:first_name => nil, :last_name => "Lockhart", :email => "email@email.com", :password => "password", :password_confirmation => "password")
+    @user = User.new(:first_name => nil, :last_name => "Lockhart", :email => "email@email.com", :password => "password1", :password_confirmation => "password1")
     expect(@user).to_not be_valid
     expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
+  end
+  
+  it "is invalid when the password is under 8 characters" do
+    @user = User.new(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "pass", :password_confirmation => "pass")
+    expect(@user).to_not be_valid
+    expect(@user.errors.full_messages[0]).to eq("Password is too short (minimum is 8 characters)")
   end
 
 
