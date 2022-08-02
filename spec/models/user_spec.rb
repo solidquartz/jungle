28972e9fbc1221de414f3420373c006a0476e3a8
Aky Mac Donald <akymacd@gmail.com>
2022-08-01 21:38:43 -0400

    Add user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe User, type: :model do
   it "is invalid when password and password_confirmation fields don't match" do
     @user = User.new(:first_name => "Tifa", :last_name => "Lockhart", :email => "email@email.com", :password => "password", :password_confirmation => "notpassword")
     expect(@user).to_not be_valid
+    expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
   end
 
-  it "must be a unique email" do
-
+  it "is invalid when the email is not unique" do
+    User.create!(:email => "email@email.com", :password => "password", :password_confirmation => "password")
+    @user = User.new(:email => "email@email.com", :password => "password", :password_confirmation => "password")
+    expect(@user).to_not be_valid
+    expect(@user.errors.full_messages[0]).to eq("Email has already been taken")
   end
 
   # it "must have a first name, last name, and email" do
